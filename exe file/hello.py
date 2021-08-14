f800604ab7f7b336d7755dc53e433e15b8b3224d
imvickykumar999 <imvickykumar999@gmail.com>
2021-08-14 19:42:55 +0530

    one file

--- exe file/hello.py
+++
@@ -1,7 +0,0 @@
-print('Hello World!')
-
-a = float(input('Enter a : '))
-b = float(input('Enter b : '))
-print('a + b = ', a+b)
-
-input('Press any button to exit.')
---
+++ pyinstaller/hello.py
@@ -0,0 +1,7 @@
+print('Hello World!')
+
+a = float(input('Enter a : '))
+b = float(input('Enter b : '))
+print('a + b = ', a+b)
+
+input('Press any button to exit.')
